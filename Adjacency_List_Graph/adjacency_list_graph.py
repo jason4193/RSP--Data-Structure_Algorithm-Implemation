2ed195efb74694ad4b55ec059d8c090ed2393efb
{jason4193} <{jason4193@gmail.com}>
2023-12-13 20:02:08 +0800

    Adding funciton of removing vertex and edge

--- Adjacency_List_Graph/adjacency_list_graph.py
+++ Adjacency_List_Graph/adjacency_list_graph.py
@@ -15,10 +15,12 @@ class Vertex:
 
 # Graph Class
 # Graph class contain a list of vertices
-# Time Complexity: check_same_name() -> O(n) : check if a vertex is already exist
-#                  add_vertex() -> O(n) : append a vertex by calling check_same_name()
-#                  add_edge() -> O(n) : append an edge by calling add_neighbor()
-#                  print_graph() -> O(n) : print all vertices and their neighbors
+# Time Complexity: check_same_name() -> O(V) : check if a vertex is already exist
+#                  add_vertex() -> O(V) : append a vertex by calling check_same_name()
+#                  add_edge() -> O(V) : append an edge by calling add_neighbor(), require to check vertices exist so O(V)
+#                  remove_vertex() -> O(V+E) : remove a vertex by calling remove_edges() and remove the vertex from vertices list 
+#                  remove_edges() -> O(E) : remove all edges that connect to the vertex
+#                  print_graph() -> O(VE) : print all vertices and their neighbors
 class Graph:
     vertices = []
 
@@ -51,13 +53,31 @@ class Graph:
         if vertex_u is None or vertex_v is None:
             return False
         else: 
-            vertex_u.add_neighbor(v)
-            vertex_v.add_neighbor(u)
+            vertex_u.add_neighbor(vertex_v)
+            vertex_v.add_neighbor(vertex_u)
             return True
+        
+
+    def remove_vertex(self, vertex):
+        # search the vertex in vertices list
+        for v in self.vertices:
+            if v.name == vertex:
+                # remove all edges that connect to the vertex
+                self.remove_edges(v)
+                # remove the vertex from vertices list
+                self.vertices.remove(v)
+                return True
+        return False
+
+    def remove_edges(self, vertex):
+        # remove all edges that connect to the vertex
+        for neighbor in vertex.neighbors:
+                    neighbor.neighbors.remove(vertex)
 
     def print_graph(self):
         for vertex in self.vertices:
-            print(vertex.name, vertex.neighbors)
+            neighors_names = [neighbor.name for neighbor in vertex.neighbors]
+            print(vertex.name, neighors_names)
 
 
 # Driver code
@@ -78,4 +98,8 @@ if __name__ == '__main__':
     for edge in edges:
         g.add_edge(edge[0], edge[1])
     
+    g.print_graph()
+
+    g.remove_vertex('A')
+    print()
     g.print_graph()
